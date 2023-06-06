f6dd30de536f11023062e86d5b7eab73ad2a3a3c
Radu Dumitrescu <r.dumitrescu@reply.com>
2023-06-06 21:01:40 +0300

    added a little design

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,5 @@
 import "bootstrap/dist/css/bootstrap.min.css";
-import { Input } from "reactstrap";
+import { Button, Col, Container, FormGroup, Input, Label, Row } from "reactstrap";
 import { useState } from "react";
 import { Movie, MovieRecommendationDetails, MovieService } from "./sdk/movieService.sdk";
 
@@ -11,62 +11,66 @@ export default function App() {
   const [userDescription, setUserDescription] = useState<string>("");
 
   return (
-    <div>
-      <div>
-        Say a fews words about yourself:
-        <Input onChange={
-          (e) => {
-            setUserDescription(e.target.value);
-          }
-        }>
-        </Input>
-      </div>
-      <button onClick={async () => {
-        setMoviesLoaded(false);
-        setMovies([]);
-        setMovieRecommendationDetails([]);
-        setLoading(true);
-        const movies = await MovieService.recommendMoviesBasedOnDescription(userDescription);
-        setLoading(false);
-        
-        if (movies.length === 0) {
-          alert("An error has occured! Contact our system administrator!");
-          return;
-        }
+    <Container className="mt-5">
+      <Row className="justify-content-md-center">
+        <Col md={8}>
+          <h3>Movie Recommendation System</h3>
+          <FormGroup>
+            <Label>Say a fews words about yourself:</Label>
+            <Input onChange={
+              (e) => {
+                setUserDescription(e.target.value);
+              }
+            }>
+            </Input>
+          </FormGroup>
+          <Button
+            color="primary"
+          onClick={async () => {
+            setMoviesLoaded(false);
+            setMovies([]);
+            setMovieRecommendationDetails([]);
+            setLoading(true);
+            const movies = await MovieService.recommendMoviesBasedOnDescription(userDescription);
+            setLoading(false);
+            
+            if (movies.length === 0) {
+              alert("An error has occured! Contact our system administrator!");
+              return;
+            }
 
-        setMovies(movies);
-        setMoviesLoaded(true);
+            setMovies(movies);
+            setMoviesLoaded(true);
 
-        const result = await MovieService.getPronsAndConsForMovies(movies);
-        setMovieRecommendationDetails(result);
-      }}>Get movie recommendations</button>
-      {loading ? <div>Loading...</div> : <div></div>}
-      {moviesLoaded ? 
-      <div>
-      <div style={{ display: "flex", flexDirection: "column" }}>
-        <b>The recommended movies are:</b>
-        {movies.map((x) => <div>{`${x.title} ${x.releaseDate}`}</div>)}
-      </div>
-      <b>
-        Let's see what people say about them:
-        </b>
-      </div>
-      :
-      <div></div>
-      }
+            const result = await MovieService.getPronsAndConsForMovies(movies);
+            setMovieRecommendationDetails(result);
+          }}>Get movie recommendations</Button>
+          {loading ? <div>Loading...</div> : <div></div>}
+          {moviesLoaded ? 
+            (<div>
+              <div style={{ display: "flex", flexDirection: "column" }} className="mt-3">
+                <b>The recommended movies are:</b>
+                {movies.map((x) => <div>{`${x.title} ${x.releaseDate}`}</div>)}
+              </div>
+              <div className="mt-3"><b>Let's see what people say about them:</b></div>
+              {movieRecommendationDetails.length === 0 ? (<div>Loading...</div>) : (<div></div>)}
+            </div>) : (<div></div>)}
+          
 
-      <div style={{ display: "flex", flexDirection: "column" }}>
-        {movieRecommendationDetails.map((x) =>
-          <div>
-            <h2>{`${x.title}`}</h2>
-            <h4>Positives</h4>
-            <div>{`${x.advantages}`}</div>
-            <h4>Negatives</h4>
-            <div>{`${x.disadvantages}`}</div>
+          <div style={{ display: "flex", flexDirection: "column" }}>
+            {movieRecommendationDetails.map((x) =>
+              <div className="mt-3">
+                <h3>{`${x.title}`}</h3>
+                <h4>Positives</h4>
+                <div>{`${x.advantages}`}</div>
+                <h4>Negatives</h4>
+                <div>{`${x.disadvantages}`}</div>
+              </div>
+            )}
           </div>
-        )}
-      </div>
-    </div>
+        </Col>
+      </Row>
+    </Container>
   );
 
 }
