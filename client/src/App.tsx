599e56fe316af0397cadd0be62c03b7a43003645
Virgil993 <turcu.virgil@gmail.com>
2024-01-30 15:06:56 +0200

    Update to use workspaces and vite for the client

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,28 @@
-import "bootstrap/dist/css/bootstrap.min.css";
-import { Button, Col, Container, FormGroup, Input, Label, Row } from "reactstrap";
 import { useState } from "react";
-import { Movie, MovieRecommendationDetails, MovieService } from "./sdk/movieService.sdk";
+import "bootstrap/dist/css/bootstrap.min.css";
+import {
+  Button,
+  Col,
+  Container,
+  FormGroup,
+  Input,
+  Label,
+  Row,
+} from "reactstrap";
+import {
+  MovieRecommendationDetails,
+  Movie,
+  MovieService,
+} from "@genezio-sdk/movie-guru_us-east-1";
+import "./App.css";
 
-export default function App() {
+function App() {
   const [loading, setLoading] = useState<boolean>(false);
   const [moviesLoaded, setMoviesLoaded] = useState<boolean>(false);
   const [movies, setMovies] = useState<Movie[]>([]);
-  const [movieRecommendationDetails, setMovieRecommendationDetails] = useState<MovieRecommendationDetails[]>([]);
+  const [movieRecommendationDetails, setMovieRecommendationDetails] = useState<
+    MovieRecommendationDetails[]
+  >([]);
   const [userDescription, setUserDescription] = useState<string>("");
 
   return (
@@ -17,48 +32,68 @@ export default function App() {
           <h3>Movie Recommendation System</h3>
           <FormGroup>
             <Label>Say a fews words about yourself:</Label>
-            <Input onChange={
-              (e) => {
+            <Input
+              onChange={(e) => {
                 setUserDescription(e.target.value);
-              }
-            }>
-            </Input>
+              }}
+            ></Input>
           </FormGroup>
           <Button
             color="primary"
-          onClick={async () => {
-            setMoviesLoaded(false);
-            setMovies([]);
-            setMovieRecommendationDetails([]);
-            setLoading(true);
-            const movies = await MovieService.recommendMoviesBasedOnDescription(userDescription);
-            setLoading(false);
+            onClick={async () => {
+              setMoviesLoaded(false);
+              setMovies([]);
+              setMovieRecommendationDetails([]);
+              setLoading(true);
+              const movies =
+                await MovieService.recommendMoviesBasedOnDescription(
+                  userDescription
+                );
+              setLoading(false);
 
-            if (movies.length === 0) {
-              alert("An error has occured! Contact our system administrator!");
-              return;
-            }
+              if (movies.length === 0) {
+                alert(
+                  "An error has occured! Contact our system administrator!"
+                );
+                return;
+              }
 
-            setMovies(movies);
-            setMoviesLoaded(true);
+              setMovies(movies);
+              setMoviesLoaded(true);
 
-            const result = await MovieService.getProsAndConsForMovies(movies);
-            setMovieRecommendationDetails(result);
-          }}>Get movie recommendations</Button>
+              const result = await MovieService.getProsAndConsForMovies(movies);
+              setMovieRecommendationDetails(result);
+            }}
+          >
+            Get movie recommendations
+          </Button>
           {loading ? <div>Loading...</div> : <div></div>}
-          {moviesLoaded ?
-            (<div>
-              <div style={{ display: "flex", flexDirection: "column" }} className="mt-3">
+          {moviesLoaded ? (
+            <div>
+              <div
+                style={{ display: "flex", flexDirection: "column" }}
+                className="mt-3"
+              >
                 <b>The recommended movies are:</b>
-                {movies.map((x) => <div>{`${x.title} ${x.releaseDate}`}</div>)}
+                {movies.map((x) => (
+                  <div>{`${x.title} ${x.releaseDate}`}</div>
+                ))}
               </div>
-              <div className="mt-3"><b>Let's see what people say about them:</b></div>
-              {movieRecommendationDetails.length === 0 ? (<div>Loading...</div>) : (<div></div>)}
-            </div>) : (<div></div>)}
-
+              <div className="mt-3">
+                <b>Let's see what people say about them:</b>
+              </div>
+              {movieRecommendationDetails.length === 0 ? (
+                <div>Loading...</div>
+              ) : (
+                <div></div>
+              )}
+            </div>
+          ) : (
+            <div></div>
+          )}
 
           <div style={{ display: "flex", flexDirection: "column" }}>
-            {movieRecommendationDetails.map((x) =>
+            {movieRecommendationDetails.map((x) => (
               <div className="mt-3">
                 <h3>{`${x.title}`}</h3>
                 <h4>Positives</h4>
@@ -66,11 +101,12 @@ export default function App() {
                 <h4>Negatives</h4>
                 <div>{`${x.disadvantages}`}</div>
               </div>
-            )}
+            ))}
           </div>
         </Col>
       </Row>
     </Container>
   );
-
 }
+
+export default App;
--- server/movie.ts
+++ server/movie.ts
@@ -1,41 +1,43 @@
-import { ChatCompletionRequestMessageRoleEnum, Configuration, OpenAIApi } from "openai";
-import dotenv from "dotenv";
+import {
+  ChatCompletionRequestMessageRoleEnum,
+  Configuration,
+  OpenAIApi,
+} from "openai";
 import axios from "axios";
-dotenv.config();
+import { GenezioDeploy } from "@genezio/types";
 
 // --- Types ---
 
 export type Movie = {
-  title: string,
-  releaseDate: string,
-}
+  title: string;
+  releaseDate: string;
+};
 
 export type MovieRecommendationDetails = {
-  title: string,
-  advantages: string,
-  disadvantages: string,
-}
+  title: string;
+  advantages: string;
+  disadvantages: string;
+};
 
 // --- Constants ---
 
-const movieRecommendationPrompt = (userDescription: string) => (
+const movieRecommendationPrompt = (userDescription: string) =>
   // TODO 1: Write the prompt for the movie recommendation here.
-  ``);
+  ``;
 
-const reviewSummaryPrompt = (reviews: string[]) => (
+const reviewSummaryPrompt = (reviews: string[]) =>
   // TODO 3: Write the prompt for the review summary here.
-  ``
-);
-
+  ``;
 /**
  * The Movie class that will be deployed on the genezio infrastructure.
  */
+@GenezioDeploy()
 export class MovieService {
   openai: OpenAIApi;
 
   constructor() {
     const configuration = new Configuration({
-      apiKey: process.env.OPENAI_SECRET_KEY
+      apiKey: process.env.OPENAI_SECRET_KEY,
     });
     this.openai = new OpenAIApi(configuration);
   }
@@ -51,14 +53,16 @@ export class MovieService {
    *          Each Movie object in the array represents a recommended movie that
    *          matches the user's description.
    */
-  async recommendMoviesBasedOnDescription(userDescription: string): Promise<Movie[]> {
+  async recommendMoviesBasedOnDescription(
+    userDescription: string
+  ): Promise<Movie[]> {
     const prompt = movieRecommendationPrompt(userDescription);
     // TODO 2: Make a call to the OpenAI API to generate movie recommendations based on the user's description
     // Parse and return the results as an array of Movie objects.
 
     const response = await this.openai.createChatCompletion({
-      model: 'gpt-3.5-turbo',
-      temperature: 0.5,    // This check is used to prevent prompt injection attacks.
+      model: "gpt-3.5-turbo",
+      temperature: 0.5, // This check is used to prevent prompt injection attacks.
 
       messages: [
         {
@@ -69,17 +73,24 @@ export class MovieService {
       max_tokens: 1024,
     });
 
-
     // TODO 2.1 Check if the response contains the expected output format.
     // This check is used to prevent prompt injection attacks.
-    if (response.data && response.data.choices && response.data.choices.length > 0 && response.data.choices[0].message) {
+    if (
+      response.data &&
+      response.data.choices &&
+      response.data.choices.length > 0 &&
+      response.data.choices[0].message
+    ) {
       try {
-        const movies = JSON.parse(response.data.choices[0].message.content);
+        const movies = JSON.parse(response.data.choices[0].message.content!);
 
-        console.log("Get movie recommendations based on description done.")
+        console.log("Get movie recommendations based on description done.");
         return movies;
       } catch (e) {
-        console.error("Error parsing movie recommendations", response.data.choices[0].message.content);
+        console.error(
+          "Error parsing movie recommendations",
+          response.data.choices[0].message.content
+        );
         return [];
       }
     }
@@ -96,16 +107,14 @@ export class MovieService {
    *          Each MovieRecommendationDetails object in the array provides detailed information
    *          about the pros and cons of the corresponding Movie object from the input array.
    */
-  async getProsAndConsForMovies(movies: Movie[]): Promise<MovieRecommendationDetails[]> {
-    const prosAndCons = movies
-      .map((x: Movie) =>
-        this.#getMovieReview(x.title)
-          .then(async (reviews: string[]) => {
-            return await this.#getReviewSummary(reviews);
-          }
-          )
-      );
-
+  async getProsAndConsForMovies(
+    movies: Movie[]
+  ): Promise<MovieRecommendationDetails[]> {
+    const prosAndCons = movies.map((x: Movie) =>
+      this.#getMovieReview(x.title).then(async (reviews: string[]) => {
+        return await this.#getReviewSummary(reviews, x.title);
+      })
+    );
 
     const result = await Promise.all(prosAndCons);
 
@@ -123,14 +132,14 @@ export class MovieService {
   async #getMovieReviews(id: string): Promise<string[]> {
     const url = `https://api.themoviedb.org/3/movie/${id}/reviews?language=en-US&page=1`;
     const options = {
-      method: 'GET',
+      method: "GET",
       headers: {
-        accept: 'application/json',
-        Authorization: `Bearer ${process.env.TMDB_API_KEY}`
-      }
+        accept: "application/json",
+        Authorization: `Bearer ${process.env.TMDB_API_KEY}`,
+      },
     };
 
-    const response = await axios(url, options)
+    const response = await axios(url, options);
 
     return response.data.results.map((x: any) => x.content).slice(0, 3);
   }
@@ -138,22 +147,28 @@ export class MovieService {
   async #getMovieId(title: string): Promise<string> {
     const url = `https://api.themoviedb.org/3/search/movie?query=${title}&include_adult=false&language=en-US&page=1`;
     const options = {
-      method: 'GET',
+      method: "GET",
       headers: {
-        accept: 'application/json',
-        Authorization: `Bearer ${process.env.TMDB_API_KEY}`
-      }
+        accept: "application/json",
+        Authorization: `Bearer ${process.env.TMDB_API_KEY}`,
+      },
     };
 
-    const response = await axios(url, options)
-    if (response.data.results.length === 0 || response.data.results[0].id === undefined) {
+    const response = await axios(url, options);
+    if (
+      response.data.results.length === 0 ||
+      response.data.results[0].id === undefined
+    ) {
       throw new Error(`Movie with title ${title} not found!`);
     }
 
     return response.data.results[0].id;
   }
 
-  async #getReviewSummary(reviews: string[]): Promise<MovieRecommendationDetails> {
+  async #getReviewSummary(
+    reviews: string[],
+    title: string
+  ): Promise<MovieRecommendationDetails> {
     const prompt = reviewSummaryPrompt(reviews);
     // TODO 4: Make a call to the OpenAI API to generate a summary of the reviews
     // Parse the response and return the results as a string.
@@ -162,9 +177,9 @@ export class MovieService {
     // This check is used to prevent prompt injection attacks.
 
     return {
-      title: "",
+      title: title,
       advantages: "",
-      disadvantages: ""
-    }
+      disadvantages: "",
+    };
   }
 }
